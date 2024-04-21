85c2b856d2de1c6b51d0a580af460fbb41d0dcf1
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-21 11:05:13 +0300

    add react and bootstrap to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,24 @@
 module.exports = {
   renderer: {
-    entry: './src/renderer/javascripts/index.js'
+    entry: './src/renderer/javascripts/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.js?$/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              exclude: /node_modules/,
+              presets: ['@babel/preset-react']
+            }
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: ["style-loader", "css-loader", 'sass-loader'],
+        }
+      ]
+    }
   },
   preload: {
     entry: './src/preload/index.js'
