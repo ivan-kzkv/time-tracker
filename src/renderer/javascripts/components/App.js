4f0084ee27f281ac7c475f355b62c594514795f9
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-21 11:05:51 +0300

    create react template and root component

---
+++ src/renderer/javascripts/components/App.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const App = () => {
+    return <h1>Hello World 2</h1>;
+}
--- src/renderer/javascripts/index.js
+++ src/renderer/javascripts/index.js
@@ -1,7 +1,10 @@
-require('application.css')
+import React from "react";
+import {createRoot}  from 'react-dom/client';
+import {App} from "./components/App";
+import '../stylesheets/bootstrap.min.css';
 
-window.MessagesAPI.onLoaded((_, data) => {
-  document.getElementById('title').innerHTML = data.appName + ' App'
-  document.getElementById('details').innerHTML = 'built with Electron v' + data.electronVersion
-  document.getElementById('versions').innerHTML = 'running on Node v' + data.nodeVersion + ' and Chromium v' + data.chromiumVersion
-})
+// Render your React component instead
+const root = createRoot(document.getElementById('app'));
+root.render(<App/>);
+
+
