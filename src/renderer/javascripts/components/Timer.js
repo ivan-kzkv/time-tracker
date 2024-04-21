5f9b771c629fd87a399804259bfdf057d945e92b
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-21 19:55:57 +0300

    [renderer] create timer template

--- src/renderer/javascripts/components/Timer.js
+++ src/renderer/javascripts/components/Timer.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 export const Timer = () => {
+    const [isStarted, setIsStarted] = useState(false);
+    
     return (
-        <h2>I'm TIMER</h2>
+        <div className="row mt-3 p-1 d-flex align-items-center">
+            <div className="col-8">
+                <div className="form-floating mb-3">
+                    <input type="text" className="form-control" id="Task" placeholder="Enter task"/>
+                    <label htmlFor="Task">Task name</label>
+                </div>
+            </div>
+            <div className="col-2">
+                <div>00:00:00</div>
+            </div>
+            <div className="col-2">
+                <div>
+                    {
+                        isStarted ? <button type="button" className="btn btn-danger" onClick={() => setIsStarted(false)}>Stop</button> :
+                            <button type="button" className="btn btn-success" onClick={() => setIsStarted(true)}>Play</button>
+                    }
+                </div>
+            </div>
+        </div>
     );
 }
