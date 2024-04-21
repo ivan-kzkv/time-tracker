fc3044b43708b1b82bc1494e85a95b9048ed833e
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-21 20:30:13 +0300

    [renderer] update list teplate with bootstrap classes

--- src/renderer/javascripts/components/ListRecords.js
+++ src/renderer/javascripts/components/ListRecords.js
@@ -3,15 +3,11 @@ import {Record} from "./Record";
 
 export const ListRecords = () => {
     return (
-        <div>
-            <h2>I'm Record list</h2>
-            <ul>
-                <li key={1}>
-                    <Record/>
-                </li>
-                <li key={2}>
-                    <Record/>
-                </li>
+        <div className="ms-1 me-1">
+            <ul className="list-group">
+                {
+                    [<Record/>, <Record/>].map((el, i) => <li key={i} className="list-group-item">el</li>)
+                }
             </ul>
         </div>
     );
