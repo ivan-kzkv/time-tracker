35ca6dcd3b8a9c4e92367054cc9becdbf4f85c2c
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-26 21:29:29 +0300

    [preload] rename parameter name in createProject event

--- src/preload/index.js
+++ src/preload/index.js
@@ -2,5 +2,5 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('MessagesAPI', {
       openModal: callback => ipcRenderer.on('open-create-project-modal', callback),
-      createProject: name => ipcRenderer.invoke('createProject', name)
+      createProject: projectData => ipcRenderer.invoke('createProject', projectData)
 });
