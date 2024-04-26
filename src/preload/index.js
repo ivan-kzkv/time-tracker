6302375cdef9654063642497a70859754cc40b7c
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-26 18:10:26 +0300

    [preload] extend MessageAPI with method to create project

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,5 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('MessagesAPI', {
-  openModal: callback => ipcRenderer.on('open-create-project-modal', callback)
-})
+      openModal: callback => ipcRenderer.on('open-create-project-modal', callback),
+      createProject: name => ipcRenderer.invoke('createProject', name)
+});
