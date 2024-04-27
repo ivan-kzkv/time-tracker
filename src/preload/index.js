4b8d26afe14c78bb3bf56d4fff81ac469cb367fe
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 18:22:43 +0300

    [preload] add bridge for load tasks list event

--- src/preload/index.js
+++ src/preload/index.js
@@ -2,5 +2,6 @@ import { contextBridge, ipcRenderer } from 'electron';
 
 contextBridge.exposeInMainWorld('MessagesAPI', {
       openModal: callback => ipcRenderer.on('open-create-project-modal', callback),
-      createProject: projectData => ipcRenderer.invoke('createProject', projectData)
+      createProject: projectData => ipcRenderer.invoke('createProject', projectData),
+      loadTasks: queryParams => ipcRenderer.invoke('loadTasks', {queryParams})
 });
