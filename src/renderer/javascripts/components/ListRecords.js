37e80fd71d3fcdf1f48197fec322af6cb89b9505
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 19:06:51 +0300

    [renderer] ListRecords.js sort tasks by id from bigger to smaller

--- src/renderer/javascripts/components/ListRecords.js
+++ src/renderer/javascripts/components/ListRecords.js
@@ -12,7 +12,8 @@ export const ListRecords = ({activeProjectId}) => {
         <div className="ms-1 me-1">
             {
                 records.length ? <ul className="list-group">{
-                        records.map((el, i) => <li key={i} className="list-group-item">{<Record/>}</li>)
+                        records.sort((a, b) => b.id - a.id)
+                            .map((el, i) => <li key={i} className="list-group-item">{<Record taskName={el.name} seconds={el.time}/>}</li>)
                     }
                 </ul> 
                     : <h2 className="text-center bg-light">No register tasks</h2>
