1a48b77705624957a9cccd320095e3a55c586cb4
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 19:00:11 +0300

    [renderer] ListRecords.js change parameter name, update template to no tasks placeholder

--- src/renderer/javascripts/components/ListRecords.js
+++ src/renderer/javascripts/components/ListRecords.js
@@ -1,22 +1,24 @@
 import React, {useEffect, useState} from "react";
 import {Record} from "./Record";
 
-export const ListRecords = ({activeProject}) => {
+export const ListRecords = ({activeProjectId}) => {
     const [records, setRecords] = useState([]);
     useEffect(() => {
-        window.MessagesAPI.loadTasks({projectId: activeProject.id})
-            .then(tasksList => {
-                setRecords(tasksList);
-            })
-    }, [activeProject]);
+        window.MessagesAPI.loadTasks({project_id: activeProjectId})
+            .then(tasksList => setRecords(tasksList))
+    }, [activeProjectId]);
     
     return (
         <div className="ms-1 me-1">
-            <ul className="list-group">
-                {
-                    records.map((el, i) => <li key={i} className="list-group-item">{<Record/>}</li>)
-                }
-            </ul>
+            {
+                records.length ? <ul className="list-group">{
+                        records.map((el, i) => <li key={i} className="list-group-item">{<Record/>}</li>)
+                    }
+                </ul> 
+                    : <h2 className="text-center bg-light">No register tasks</h2>
+            }
+            
+            
         </div>
     );
 }
