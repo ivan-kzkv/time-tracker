fe41fdf9ca271902114a7fa0e73606bb179df615
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 18:24:22 +0300

    [renderer] update ListRecords.js to load task list on init and active project change in useEffect

--- src/renderer/javascripts/components/ListRecords.js
+++ src/renderer/javascripts/components/ListRecords.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {Record} from "./Record";
 
-export const ListRecords = () => {
+export const ListRecords = ({activeProject}) => {
+    const [records, setRecords] = useState([]);
+    useEffect(() => {
+        window.MessagesAPI.loadTasks({projectId: activeProject.id})
+            .then(tasksList => {
+                setRecords(tasksList);
+            })
+    }, [activeProject]);
+    
     return (
         <div className="ms-1 me-1">
             <ul className="list-group">
                 {
-                    [<Record/>, <Record/>].map((el, i) => <li key={i} className="list-group-item">{el}</li>)
+                    records.map((el, i) => <li key={i} className="list-group-item">{<Record/>}</li>)
                 }
             </ul>
         </div>
