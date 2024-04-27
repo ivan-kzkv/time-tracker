779c757fbcecf3bb84d42f29d259f39e07f9a9e9
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 19:13:57 +0300

    [renderer] Timer.js handle inserting task name

--- src/renderer/javascripts/components/Timer.js
+++ src/renderer/javascripts/components/Timer.js
@@ -3,12 +3,21 @@ import {Time} from "./Time";
 
 export const Timer = () => {
     const [isStarted, setIsStarted] = useState(false);
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [taskName, setTaskName] = useState('');
+    
+    const updateTaskName = (event) => {
+        const taskName = event.target.value;
+        setTaskName(taskName);
+        setButtonDisabled(!taskName)
+    }
+    
     
     return (
         <div className="row mt-3 p-1 d-flex align-items-center">
             <div className="col-8">
                 <div className="form-floating mb-3">
-                    <input type="text" className="form-control" id="Task" placeholder="Enter task"/>
+                    <input type="text" className="form-control" id="Task" placeholder="Enter task" value={taskName} onChange={updateTaskName}/>
                     <label htmlFor="Task">Task name</label>
                 </div>
             </div>
@@ -19,7 +28,7 @@ export const Timer = () => {
                 <div>
                     {
                         isStarted ? <button type="button" className="btn btn-danger" onClick={() => setIsStarted(false)}>Stop</button> :
-                            <button type="button" className="btn btn-success" onClick={() => setIsStarted(true)}>Play</button>
+                            <button type="button" className="btn btn-success" onClick={() => setIsStarted(true)} disabled={buttonDisabled}>Play</button>
                     }
                 </div>
             </div>
