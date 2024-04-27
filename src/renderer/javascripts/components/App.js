f922227348a792df84b1246fe85718ea8d0f0f95
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 16:19:27 +0300

    [renderer] in App.js add onCreateProject handler and procced with set this project to local storage

--- src/renderer/javascripts/components/App.js
+++ src/renderer/javascripts/components/App.js
@@ -2,12 +2,19 @@ import React, {useState} from "react";
 import {CreateProjectModal} from "./create-project-modal/CreateProjectModal";
 import {Timer} from "./Timer";
 import {ListRecords} from "./ListRecords";
+import {setActiveProject} from "../utils/localStorage.handle";
 
 export const App = () => {
     const [modalOpened, setModalOpened] = useState(false);
     
     const openCreateProjectModal = () => setModalOpened(true);
-    
+    const onCreateProject = (newProject) => {
+        setModalOpened(false);
+        setActiveProject(newProject);
+        // TODO set active project in main window
+        
+        // TODO clear Old session and begin new One
+    }
     
     window.MessagesAPI.openModal(openCreateProjectModal);
     
@@ -16,7 +23,9 @@ export const App = () => {
         <div>
             {
                 modalOpened ? 
-                    <CreateProjectModal onCloseModal={() => setModalOpened(false)}/> :
+                    <CreateProjectModal 
+                        onCloseModal={() => setModalOpened(false)}
+                        onCreateProject={onCreateProject}/> :
                     <div>
                         <Timer/>
                         <hr/>
