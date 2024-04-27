fa465dc3ffacdce71f55f440a8f62fd4a529fae6
Ivan Kozikov <kozikov.work@gmail.com>
2024-04-27 19:35:43 +0300

    [preload] add listeners to start and stopTimer

--- src/preload/index.js
+++ src/preload/index.js
@@ -3,5 +3,10 @@ import { contextBridge, ipcRenderer } from 'electron';
 contextBridge.exposeInMainWorld('MessagesAPI', {
       openModal: callback => ipcRenderer.on('open-create-project-modal', callback),
       createProject: projectData => ipcRenderer.invoke('createProject', projectData),
-      loadTasks: queryParams => ipcRenderer.invoke('loadTasks', {queryParams})
+      loadTasks: queryParams => ipcRenderer.invoke('loadTasks', {queryParams}),
+      startTimer: timerListener => {
+            ipcRenderer.invoke('startTimer');
+            ipcRenderer.on('timer-ping', timerListener);
+      },
+      stopTimer: callback => ipcRenderer.on('stopTimer', callback)
 });
